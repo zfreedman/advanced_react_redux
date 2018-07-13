da09581327ba93c3d1ef7a3966eafcbe5d7eaed5
zfreedman <zfreedman92@gmail.com>
2018-07-13 13:03:35 -0700

    feat: Recreated prod/testing/src directory from ground up

---
+++ prod/testing/src/components/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CommentList from "./commentList";
+import CommentBox from "./commentBox";
+
+export default () => {
+  return (
+    <div>
+      app component
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+};
