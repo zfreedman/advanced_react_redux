1270cb9ab547e6df055c2d9049c38641d2eab24f
zfreedman <zfreedman92@gmail.com>
2018-07-14 15:34:50 -0700

    test: Refactored original test to now use enzyme.shallow

--- prod/testing/src/components/__tests__/app.test.js
+++ prod/testing/src/components/__tests__/app.test.js
@@ -1,11 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
 
 import App from "../app";
+import CommentBox from "../commentBox";
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain("comment box");
-  ReactDOM.unmountComponentAtNode(div);
+  // without enzyme
+  // const div = document.createElement("div");
+  // ReactDOM.render(<App />, div);
+  // expect(div.innerHTML).toContain("comment box");
+  // ReactDOM.unmountComponentAtNode(div);
+
+  // with enzyme
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
