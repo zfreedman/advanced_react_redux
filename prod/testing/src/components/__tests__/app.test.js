961ce40589b429c70dc62ed72cb35704128c5ecc
zfreedman <zfreedman92@gmail.com>
2018-07-14 16:09:49 -0700

    feat: Added beforeEach implementation to app.test.js

--- prod/testing/src/components/__tests__/app.test.js
+++ prod/testing/src/components/__tests__/app.test.js
@@ -6,6 +6,12 @@ import App from "components/app";
 import CommentBox from "components/commentBox";
 import CommentList from "components/commentList";
 
+// variables to referenced from different test scopes
+let wrapped;
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
 it ("shows a comment box", () => {
   // without enzyme
   // const div = document.createElement("div");
@@ -14,11 +20,9 @@ it ("shows a comment box", () => {
   // ReactDOM.unmountComponentAtNode(div);
 
   // with enzyme
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it ("shows a comment list", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
