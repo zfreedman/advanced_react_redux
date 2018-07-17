f8fecd0a0edbbde3f1aa8366269ebf496d33691e
zfreedman <zfreedman92@gmail.com>
2018-07-17 12:33:12 -0700

    test: Added textarea edit tests for <CommentBox />

--- prod/testing/src/components/__tests__/commentBox.test.js
+++ prod/testing/src/components/__tests__/commentBox.test.js
@@ -2,10 +2,40 @@ import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "components/commentBox";
 
+let wrapped;
+beforeEach(() => {
+  // full dom render
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it("has a text area and a button", () => {
-  // render component on fake DOM
-  const wrapped = mount(<CommentBox />);
-  
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
+});
+
+describe("text area edit tests", () => {
+  const input = "new comment";
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: {value: input}
+    });
+    wrapped.update();
+  });
+
+  it("has a text area users can type in", () => {
+    // assert
+    // note: using the old reference to textarea forces this test to fail
+    expect(wrapped.find("textarea").prop("value")).toEqual(input);
+  });
+
+  it("has a submittable form and the text area empties", () => {
+    wrapped.find("form").simulate("submit", {});
+    wrapped.update();
+
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
