e58715f6e6b03a3aa88bf1ac05eec248dfccbb11
zfreedman <zfreedman92@gmail.com>
2018-07-28 19:50:07 -0700

    test: Added test for <CommentList /> to ensure that comments render correct text

--- prod/testing/src/components/__tests__/commentList.test.js
+++ prod/testing/src/components/__tests__/commentList.test.js
@@ -24,3 +24,10 @@ afterEach(() => {
 it("creates one <li> per comment", () => {
   expect(wrapped.find("li").length).toEqual(initialState.comments.length);
 });
+
+it("throws the text for each comment", () => {
+  let lis = wrapped.find("li");
+  for (let i = 0; i < lis.length; ++i) {
+    expect(lis.get(i).props.children).toEqual(initialState.comments[i]);
+  }
+});
